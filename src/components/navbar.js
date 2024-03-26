2abc011811a2a4ad1c1d6cdb729e169aff16d921
anastasiafff <160554698+anastasiafff@users.noreply.github.com>
2024-03-26 16:39:43 +0700

    Restructure

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.createShadowRoot();
+        this.render();
+    }
+
+    createShadowRoot() {
+        this.attachShadow({ mode: 'open' });
+    }
+
+    render() {
+        this.shadowRoot.innerHTML = `
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+            </div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+                <li><a href="#" class="nav-link px-2">Features</a></li>
+                <li><a href="#" class="nav-link px-2">Pricing</a></li>
+                <li><a href="#" class="nav-link px-2">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2">About</a></li>
+            </ul>
+            <div class="col-md-3 text-end">
+            </div>
+        </header>`;
+    }
+}
+
+customElements.define('navbar-component', Navbar);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import tailwind from 'https://cdn.tailwindcss.com'
+// import './components/navbar.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    const navbar = document.getElementById('navbar-component');
+    // root.appendChild(navbar);
+});
